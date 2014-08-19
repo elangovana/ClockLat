ed906e8e19f8f7d871a8bebd2f838b9a86caf5c8
Aparna Elangovan <aparnaelangovan@Aparnas-MacBook-Air.local>
2014-08-19 17:30:07 +1000

    Use linear regression on train data

--- sourcecode/main.R
+++ sourcecode/main.R
@@ -1,5 +1,5 @@
 
-options(echo=TRUE)
+options(echo=FALSE)
 args<-commandArgs(trailingOnly = TRUE)
 
 ##default data set when no args provided
@@ -11,12 +11,29 @@ if (length(args) == 2) {
   print(args)
   fileTrainDataPosts <- args[0]
   filetrainDataFriends <- args[1]
+} else {
+  cat("Using Default dataset", fileTrainDataPosts)
 }
+ 
+###Colnames
+hour1 <- "Hour1"
+id <- "Id"
+hour2 <- "Hour2"
+hour3 <- "Hour3"
+lat <- "Lat"
+lon <- "Lon"
+posts <- "Posts"
+
 
 ## load data
 trainDataPosts <- read.csv(file = fileTrainDataPosts, header = TRUE)
-colnames(trainDataPosts) <- c("Id", "Hour1","Hour2" ,  "Hour3"  , "Lat", "Lon", "Posts")
-
+colnames(trainDataPosts) <- c(id, hour1,hour2, hour3, lat, lon, posts)
 head(trainDataPosts)
 
+##linear regression model
+formula <- as.formula(paste(paste(lat,"~"),paste(c(hour1,hour2, hour3, posts),collapse="+")))
+print("Formula used: ")
+formula
 
+lm_model <-lm(formula, data= trainDataPosts)
+summary(lm_model)
