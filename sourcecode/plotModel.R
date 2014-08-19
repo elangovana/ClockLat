f9184ae5d775ac971e9c42925b6af93ef90654e0
elangovan <aparnaelangovan@aparnas-air.bigpond>
2014-08-19 21:58:02 +1000

    Change polt label

--- sourcecode/plotModel.R
+++ sourcecode/plotModel.R
@@ -10,11 +10,17 @@ createDir <- function(path){
 plotModel <- function(dataset, outDir){
   createDir(outDir)
   pdf(file.path(outDir,'plots.pdf'))
-  plot(dataset[,hour1], dataset[,lat], main="Scatterplot", xlab="hour1", ylab="latitude", pch=20)
-  plot(dataset[,hour1], dataset[,lon], main="Scatterplot", xlab="hour1", ylab="longitude", pch=20)
+  plot(dataset[,hour1], dataset[,lat], main="Hour1 vs Latitude", xlab="hour1", ylab="latitude", pch=20)
+  plot(dataset[,hour1], dataset[,lon], main="Hour1 vs longitude", xlab="hour1", ylab="longitude", pch=20)
   
-  plot(dataset[,hour2], dataset[,lat], main="Scatterplot", xlab="hour2", ylab="latitude", pch=20)
-  plot(dataset[,hour3], dataset[,lat], main="Scatterplot", xlab="hour3", ylab="latitude", pch=20)
-  plot(dataset[,posts], dataset[,lat], main="Scatterplot", xlab="posts", ylab="latitude", pch=20)
+  plot(dataset[,hour2], dataset[,lat], main="Hour2 Vs latitude", xlab="hour2", ylab="latitude", pch=20)
+  plot(dataset[,hour2], dataset[,lon], main="Hour2 Vs longitude", xlab="hour2", ylab="longitude", pch=20)
+  
+  plot(dataset[,hour3], dataset[,lat], main="Hour3 Vs latitude", xlab="hour3", ylab="latitude", pch=20)
+  plot(dataset[,hour3], dataset[,lon], main="Hour3 Vs longitude", xlab="hour3", ylab="longitude", pch=20)
+  
+  plot(dataset[,posts], dataset[,lat], main="posts vs latitude", xlab="posts", ylab="latitude", pch=20)
+  plot(dataset[,posts], dataset[,lon], main="posts vs Longitude", xlab="posts", ylab="latitude", pch=20)
+  
   dev.off()
 }
