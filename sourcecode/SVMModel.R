1f92164028588bb01f067e7390de2e9546a4fe6a
unknown <soundaryajytohi476@gmail.com>
2014-08-21 16:05:38 +1000

    SVM basic model

--- sourcecode/SVMModel.R
+++ sourcecode/SVMModel.R
@@ -1,1 +1,28 @@
-(#######SVM########)
+source("./globals.R")
+
+pkgInstall("e1071")
+
+#######SVM###########
+####################
+####################
+
+calcSVM<-function(postsData, testData, outDir){
+  createDir(outDir)
+  
+  factors <- paste(c(hour1),collapse="+")
+  
+  #Region
+  formula <- as.formula(paste(paste(region,"~"),factors))
+  print("Formula for region used: ")
+  print(formula)
+  svm_model<-svm(formula, data= postsData)
+  print(summary(svm_model))
+  predicted_reg <-predict(svm_model, newdata=testData)
+  
+  data <- cbind(testData, predicted_reg)
+  colnames(data) <- c(id, hour1,hour2, hour3,posts,region)
+  
+    
+  write.table(data,  file= file.path(outDir, "labelledcontinentsforTest.csv"),  row.names = FALSE, sep=",", quote = FALSE)
+  return(data)
+}
