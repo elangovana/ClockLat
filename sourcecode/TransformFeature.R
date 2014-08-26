37e1df696d81404723f469144860467a38f990d1
elangovan <aparnaelangovan@aparnas-air.bigpond>
2014-08-26 22:11:35 +1000

    removed invalid lat lon when using friends loc

--- sourcecode/TransformFeature.R
+++ sourcecode/TransformFeature.R
@@ -1,6 +1,46 @@
 source("./globals.R")
 source("./InstallPackage.R")
 
+pkgInstall("rworldmap")
+
+
+library(sp)
+library(rworldmap)
+
+coords2continent = function(points)
+{  
+  countriesSP <- getMap(resolution='low')
+  #countriesSP <- getMap(resolution='high') #you could use high res map from rworldxtra if you were concerned about detail
+  
+  
+  # converting points to a SpatialPoints object
+  # setting CRS directly to that from rworldmap
+  pointsSP = SpatialPoints(points, proj4string=CRS(proj4string(countriesSP)))  
+  
+  
+  # use 'over' to get indices of the Polygons object containing each point 
+  indices = over(pointsSP, countriesSP)
+  
+  return(indices) 
+  #indices$continent   # returns the continent (6 continent model)
+  #indices$REGION   # returns the continent (7 continent model)
+  #indices$ADMIN  #returns country name
+  #indices$ISO3 # returns the ISO3 code 
+}
+
+
+
+
+createContinents <- function(dataset){
+  indices <-  coords2continent(dataset[, c(lon, lat)])
+  
+  print(head(indices$REGION))
+  data<- cbind(dataset, indices$REGION)
+  names(data)[names(data) == 'indices$REGION'] <- region
+  
+  
+  return(data)
+}
 
 
 CleanInvalidData<- function(data){
@@ -32,7 +72,11 @@ createFriendsWeightedAvgLocation <- function(dataPosts, dataFriends, dataFriends
   if (is.null(dataFriendsPostsLoc )) {
     dataFriendsPostsLoc = dataPosts
   }
-  
+ 
+  #remove invalid coordinates
+  dataFriendsPostsLoc <- dataFriendsPostsLoc[ which(!is.na(dataFriendsPostsLoc[,region])), ]
+
+
   completedSoFar = 0
   resLat <- numeric(nrow(dataPosts))
   resCLat <- numeric(nrow(dataPosts))
@@ -93,7 +137,9 @@ transformTrainFeatures <- function(dataPosts, dataFriends, outDir){
   data <- CleanInvalidData(dataPosts)
   data <- createFeatureMaxMin(data)  
   data <- createFeatureAvgTotal(data)
+  data <- createContinents(data)
   data <- createFriendsWeightedAvgLocation(data, dataFriends)
+
   write.table(data,  file= file.path(outDir, "transformedData.csv"),  row.names = FALSE, sep=",", quote = FALSE)  
   return(data) 
 }
